7994b3772c728cd0e5ef21bdb22c2a3b54d50ea0
thiesac <thiesa.c@gmail.com>
2022-04-22 11:15:08 -0400

    index, style, início script

---
+++ script.js
@@ -0,0 +1,15 @@
+let listaNotas = [];
+let soma = 0
+
+function adicionarNota() {
+    soma = 0;
+    let nota = document.getElementById("input-nota").value;
+    
+    if (nota == "") {
+        alert("Campo não preenchido!");
+    }
+    for (let i = 0; i < listaNotas.length; i++) {
+        soma += parseFloat(listaNotas[i]);
+        alert(soma)
+    }
+}
