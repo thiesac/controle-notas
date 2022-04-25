ccbea334243e8c0ff3d41206b80dbcce353ac062
thiesac <thiesa.c@gmail.com>
2022-04-25 09:30:15 -0400

    falta maior e menor nota

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let listaNotas = [];
-let soma = 0
+let soma = 0;
 
 function adicionarNota() {
     soma = 0;
@@ -11,7 +11,39 @@ function adicionarNota() {
         listaNotas.push(nota);
         for (let i = 0; i < listaNotas.length; i++) {
             soma += parseFloat(listaNotas[i]);
-            alert(soma)
+        }
+        //imprime lista de notas na tela e limpa campo
+        document.getElementById("input-nota").value = "";
+        document.getElementById("h3Notas").innerText = listaNotas.join(" | ");
+        maiorNota();
+        calculaMedia();
+        situacaoAluno();
     }
+}
+//encontra a maior nota da lista
+function maiorNota() {
+    let listaNumeros = listaNotas.map(str => {
+        return Number(str);
+    })
+    document.getElementById("h3MaiorNota").innerText = Math.max(listaNumeros);
+}
+
+function calculaMedia() {
+    let media = (soma / listaNotas.length);
+    document.getElementById("h3Media").innerText = media.toFixed(2);
+}
+
+function situacaoAluno() {
+    let imprimeSituacao = document.getElementById("h3Situacao");
+    
+    if (calculaMedia() < 4) {
+        imprimeSituacao.innerText = "ALUNO REPROVADO";
+        imprimeSituacao.style.color = "rgb(255, 0, 0)";
+    } else if (calculaMedia() == 4 || calculaMedia() < 7) {
+        imprimeSituacao.innerText = "ALUNO DE RECUPERAÇÃO";
+        imprimeSituacao.style.color = "rgb(250, 253, 15)";
+    } else {
+        imprimeSituacao.innerText = "ALUNO APROVADO";
+        imprimeSituacao.style.color = "rgb(0,128,0)";
     }
 }
